d1038a04415f5ceed98412328864a742a1cac92e
Koller <710267212@qq.com>
2018-12-10 16:39:09 +0800

    add http lib use http ,isolate, navi.

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,11 @@ import 'package:flutter/material.dart';
 import 'dart:math';
 import 'todo.dart';
 import 'package:english_words/english_words.dart';
+import 'dart:isolate';
+import 'dart:convert';
+import 'dart:core';
+import 'package:http/http.dart' as http;
+
 
 void main() => runApp(DemoApp());
 
@@ -19,10 +24,123 @@ class DemoApp extends StatelessWidget{
           primaryColor: Colors.green
         ),
       home: RandomWidget(),
+      routes: <String,WidgetBuilder>{
+        '/listView': (BuildContext Widget) => new ListViewWidget()
+      },
+    );
+  }
+}
+
+
+class ListViewWidget extends StatefulWidget
+{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return new ListViewState();
+  }
+}
+
+class ListViewState extends State<ListViewWidget>{
+
+  List widgets = [];
+
+  @override
+  void initState()
+  {
+    super.initState();
+    loadData();
+  }
+
+  showLoadingDialog()
+  {
+    if(widgets.length == 0)
+      {
+        return true;
+      }
+      return false;
+  }
+
+  Widget getProgressDialog()
+  {
+    return new Center(child: new CircularProgressIndicator(),);
+  }
+
+  Widget getBody()
+  {
+    if(showLoadingDialog()){
+      return getProgressDialog();
+    }else
+      {
+        return getListView();
+      }
+  }
+
+  Widget getRow(int position)
+  {
+    return new Padding(padding: new EdgeInsets.all(10.0),
+    child: new Text("Row"+
+    "id ${widgets[position]['id']}" +"  ${widgets[position]["title"]}"));
+  }
+
+  Widget getListView()
+  {
+    debugPrint("${widgets.length}");
+
+    return ListView.builder(
+      itemCount: widgets.length,
+      itemBuilder: (BuildContext context,int position){
+       return getRow(position);
+    });
+  }
+
+  loadData() async{
+    ReceivePort receivePort = new ReceivePort();
+    await Isolate.spawn(dataLoader,receivePort.sendPort);
+
+    SendPort sendPort = await receivePort.first;
+
+    List msg = await sendReceive(sendPort,"https://jsonplaceholder.typicode.com/posts");
+
+    setState(() {
+      widgets = msg;
+    });
+  }
+
+  Future sendReceive(SendPort port,msg){
+    ReceivePort response = new ReceivePort();
+    port.send([msg,response.sendPort]);
+    return response.first;
+  }
+
+  static dataLoader(SendPort send) async{
+
+    ReceivePort receivePort = new ReceivePort();
+    send.send(receivePort.sendPort);
+
+    await for (var msg in receivePort){
+      String data = msg[0];
+      SendPort replyTo = msg[1];
+
+      http.Response response = await http.get(data);
+      replyTo.send(new JsonCodec().decode(response.body));
+    }
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("listView"),
+      ),
+      body: getBody(),
     );
   }
 
 }
+
 
 class RandomWidget extends StatefulWidget{
   @override
@@ -46,7 +164,8 @@ class RandomStates extends State<RandomWidget>
         appBar: AppBar(
           title: Text("Startup Name Generato"),
           actions: <Widget>[
-            new IconButton(icon: new Icon(Icons.list),onPressed: _ActionOnPress,)
+            new IconButton(icon: new Icon(Icons.list),onPressed: _ActionOnPress,),
+            new IconButton(icon:new Icon(Icons.widgets),onPressed: _ListView,)
           ],
         ),
         body: Center(
@@ -55,6 +174,11 @@ class RandomStates extends State<RandomWidget>
     );
   }
 
+
+  _ListView()
+  {
+    Navigator.of(context).pushNamed('/listView');
+  }
 
   Widget _buildSuggestions(){
     return new ListView.builder(
